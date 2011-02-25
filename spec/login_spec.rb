e0bae328950e869be62afc09a1f68223a7517c2b
Adam Goucher <adam@goucher.ca>
2011-02-25 06:17:42 -0500

    cleaning up some of the language to be more fluent

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 require 'home_page'
 
-module SauceWebsite
-  describe "Login" do
-    context "invalid password" do
-      it "prints error message", :depth => 'deep', :login => true do
+module "SauceWebsite" do
+  describe "Logging in" do
+    context "with an invalid password" do
+      it "displays an error message", :depth => 'deep', :login => true do
         @home = PageObjects::HomePage.new
         @login = @home.goto_login_form
         @login.username = "foo"
@@ -15,7 +15,7 @@ module SauceWebsite
       end
     end
     
-    context "correct password", :depth => 'shallow', :login => true do
+    context "with a correct password", :depth => 'shallow', :login => true do
       # if there is no block passed then it is 'pending
       it "goes to account page"
     end
